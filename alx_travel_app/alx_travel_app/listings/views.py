51edbd6109ddbd6c4902785ea47d79dd92a9939b
karivone <karimiyvonne19@gmail.com>
2025-06-13 14:00:15 +0300

    Creating Views and API Endpoints

--- alx_travel_app/alx_travel_app/listings/views.py
+++ alx_travel_app/alx_travel_app/listings/views.py
@@ -10,17 +10,3 @@ class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
 
-# listings/urls.py
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import ListingViewSet, BookingViewSet
-
-router = DefaultRouter()
-router.register(r'listings', ListingViewSet)
-router.register(r'bookings', BookingViewSet)
-
-urlpatterns = [
-    path('api/', include(router.urls)),
-]
-
-# Create your views here.
